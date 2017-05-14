a2cc744e75cc9f2207b3e6b6db3b540dcf907aa3
GliGli <sfx.gligli@free.fr>
2017-05-14 20:27:39 +0200

    removing useless data

--- anim_128_1/convert.sh
+++
@@ -1,9 +0,0 @@
-mogrify -ordered-dither 1x1 -monochrome -format mono *.bmp
-mogrify -ordered-dither 1x1 -monochrome -format png *.bmp
-
-../Bits2Byte/Bits2Byte fixed.bin 1234.mono
-../Bits2Byte/Bits2Byte anim1.bin 0000.mono 0001.mono 0002.mono 0003.mono 0004.mono 0005.mono 0006.mono 0007.mono
-../Bits2Byte/Bits2Byte anim2.bin 0008.mono 0009.mono 0010.mono 0011.mono 0012.mono 0013.mono 0014.mono 0015.mono
-../Bits2Byte/Bits2Byte anim3.bin 0016.mono 0017.mono 0018.mono 0019.mono 0020.mono 0021.mono 0022.mono 0023.mono
-../Bits2Byte/Bits2Byte anim4.bin 0024.mono 0025.mono 0026.mono 0027.mono 0028.mono 0029.mono 0030.mono 0031.mono
-
